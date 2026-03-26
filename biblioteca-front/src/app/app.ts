7b6debfe71b90451c577f0ab9bf579af0396b2bd
isergioam <isergioam@gmail.com>
2026-03-26 10:03:48 +0100

    Angular
    Componente header y footer añadido al proyecto biblioteca-app

--- biblioteca-front/src/app/app.ts
+++ biblioteca-front/src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header]
+  imports: [Header, Footer]
 })
 export class App {
-  nombre = 'Angular desde cero';
+
 
   listado = [
     'Node.js',
